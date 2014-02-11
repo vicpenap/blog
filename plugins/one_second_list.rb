b4882249e61e1b62d868a95b3dbfd67289d3e655
Victor Pena <vicpenap@gmail.com>
2014-02-11 23:47:57 +0100

    Improved one second parser to support year

--- plugins/one_second_list.rb
+++ plugins/one_second_list.rb
@@ -4,10 +4,19 @@ module Jekyll
             html = ""
             category = "One Second Every Day"
             context.registers[:site].categories[category].sort{ |a, b|  b <=> a }.each do |post|
-                regex = %r{(\w+)* \((?<month>\w+)\)}
-                original_post_name = post.data["title"]
-                post_name = regex.match(original_post_name)
-                post_name = post_name ? post_name[:month] : original_post_name
+                post_name = post.data["title"]
+                
+                regex = %r{(\w+)* \((?<month>\w+)[ ]?(?<year>\w+)?\)}
+                parsed_post_name = regex.match(post_name)
+
+                if parsed_post_name
+                    month = parsed_post_name[:month]
+                    post_name = month
+                    year = parsed_post_name[:year]
+                    if year
+                        post_name << " " + year
+                    end
+                end
                 html << "<li class='post'><a href='%s'>%s</a></li>\n" % [ post.url , post_name ]
             end
             html
